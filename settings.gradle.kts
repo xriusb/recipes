09f5e916a6ffe56968370ec54654cb661558d75d
Xavier Rius <xavier.rius@adevinta.com>
2023-03-22 23:10:32 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name = "recipes"
+
+include(":shared")
+project(":shared").projectDir = File("src/shared")
---
+++ src/test/java/com/xrius/recipes/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xrius.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
